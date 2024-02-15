25cf579ce2ece6bde6f48c72687975d0422cf9b9
RobinKoolhaas <robin.koolhaas@student.ucll.be>
2024-02-15 08:28:56 +0100

    Random Commit

--- 01-basic-python/04-lists/06-contains-duplicates/student.py
+++ 01-basic-python/04-lists/06-contains-duplicates/student.py
@@ -1,1 +1,12 @@
-# Write your code here
+#!/usr/bin/python3
+
+def contains_duplicates(xs: list) -> bool:
+    newList = []
+
+    for entry in xs :
+        if entry in newList:
+            return True
+        else:
+            newList.append(entry)
+
+    return False
